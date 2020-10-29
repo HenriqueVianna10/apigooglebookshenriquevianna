35c94e9da31237912960b87f7259226d86fe4fdc
Henrique Vianna <henriquev79@gmail.com>
2020-10-29 00:41:51 -0300

    adicionada barra de pesquisa

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import './App.css';
-import { InputGroup, Input, InputGroupAddon } from 'reactstrap';
+import { InputGroup, Input, InputGroupAddon, Button } from 'reactstrap';
 
 function App() {
   const mainHeader = () => {
       return (
         <div className='main-image d-flex justify-content-center align-items-center flex-column'>
             {/*overlay */}
-            <div className="filter"> </div>
-            <h1 className='display-2 text-center text-white mb-3' style={{zIndex: 1}}> 
+            <div className='filter'> </div>
+            <h1 className='display-2 text-center text-white mb-3' 
+                style={{zIndex: 2}}
+            > 
                Biblioteca Google
             </h1>
+            <div style={ { width: '60%', zIndex: 2 }}>
+              <InputGroup size='lg' className='mb-3'>
+                <input placeholder='Procurar Livro'/>
+              </InputGroup>
+              <InputGroupAddon addonType='append'>
+                  <Button color='secondary'>
+                    <i className='fas fa-search'></i>
+                  </Button>
+              </InputGroupAddon>
+            </div>
         </div>
       );
   }
