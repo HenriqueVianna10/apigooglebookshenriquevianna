0801e7180f7eb77d740f6ac5583a5b9dfac6fa8b
Henrique Vianna <henriquev79@gmail.com>
2020-10-29 00:20:55 -0300

    adicionada tela de fundo com imagem e texto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css';
+import { InputGroup, Input, InputGroupAddon } from 'reactstrap';
 
 function App() {
+  const mainHeader = () => {
+      return (
+        <div className='main-image d-flex justify-content-center align-items-center flex-column'>
+            {/*overlay */}
+            <div className="filter"> </div>
+            <h1 className='display-2 text-center text-white mb-3' style={{zIndex: 1}}> 
+               Biblioteca Google
+            </h1>
+        </div>
+      );
+  }
+
   return (
     <div >
-      
+      {mainHeader()}
     </div>
   );
 }
