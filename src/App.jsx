c8976cf13e69e83c9fc5bfdf5e73d9b433e3250d
Henrique Vianna <henriquev79@gmail.com>
2020-10-29 01:22:46 -0300

    instalado axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
 import './App.css';
 import { InputGroup, Input, InputGroupAddon, Button, FormGroup, Label } from 'reactstrap';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 function App() {
   const [maxResults, setMaxResults] = useState(10);
   const [startIndex, setStartIndex] = useState(1);
   const [query, setQuery] = useState('');
-  
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {
+    setLoading(true);
+    if (maxResults > 40 || maxResults < 1) {
+      toast.error('O resultado deve estar entre 1 e 40');
+
 
   const mainHeader = () => {
       return (
@@ -20,7 +28,11 @@ function App() {
             </h1>
             <div style={ { width: '60%', zIndex: 2 }}>
               <InputGroup size='lg' className='mb-3'>
-                <input placeholder='Procurar Livro'/>
+                <input 
+                          placeholder='Procurar Livro'
+                          value={query}
+                          onChange={e => setQuery(e.target.value)}
+                />
               </InputGroup>
               <InputGroupAddon addonType='append'>
                   <Button color='secondary'>
@@ -29,13 +41,23 @@ function App() {
               </InputGroupAddon>
               <div className='d-flex text-white justify-content-center' > 
                 <FormGroup className='ml-5'>
-                    <Label for='maxResults'>Quantidade Máxima de Retornos</Label>
-                    <input type='number' id='maxResults' placeholder='Quantidade Máxima de Retornos' />
+                          <Label for='maxResults'>Quantidade Máxima de Retornos</Label>
+                          <input type='number' 
+                          id='maxResults' 
+                          placeholder='Quantidade Máxima de Retornos' 
+                          value={maxResults}
+                          onChange={e => setMaxResults(e.target.value)}
+                          />
                 </FormGroup>
 
                 <FormGroup className='ml-5'>
                     <Label for='startIndex'>Indice Inicial</Label>
-                    <input type='number' id='startIndex' placeholder='Indice Inicial' />
+                    <input type='number' 
+                           id='startIndex'
+                           placeholder='Indice Inicial' 
+                           value={startIndex}
+                           onChange={e => setStartIndex(e.target.value)}
+                           />
                 </FormGroup>
               </div>
             </div>
@@ -44,10 +66,12 @@ function App() {
   }
 
   return (
-    <div >
+    <div className='w-100 h-100'>
       {mainHeader()}
+      <ToastContainer />
     </div>
   );
 }
 
 export default App;
+
